f46cc9ff167fc9502bbdb410f11d674ebb88499c
araikumatan <272487332+araikumatan@users.noreply.github.com>
2026-08-02 08:36:42 +0900

    Add files via upload

---
+++ index-B8f0ZpIx.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/ear100/favicon.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ear100 - 聞き取れる耳をつくる最初の100</title>
+    <script type="module" crossorigin src="/ear100/assets/index-B8f0ZpIx.js"></script>
+    <link rel="stylesheet" crossorigin href="/ear100/assets/index-D12BfMP3.css">
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
